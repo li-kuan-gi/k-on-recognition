08a6324be364bea5cbeeccdfda9c3affac2cf779
li-kuan-gi <82380617+li-kuan-gi@users.noreply.github.com>
2022-06-19 10:45:26 +0000

    change model to mobilenet v3 large

--- model-train/model.py
+++ model-train/model.py
@@ -3,7 +3,8 @@ from torchvision import models
 
 
 def get_model(output_size):
-    model = models.quantization.mobilenet_v3_large(pretrained=True)
+    model = models.mobilenet_v3_large(
+        pretrained=True, width_mult=1.0,  reduced_tail=False, dilated=False)
 
     n_feats = model.classifier[-1].in_features
     model.classifier[-1] = torch.nn.Linear(n_feats, output_size)
