1624a3b35d4a52358bc0dcdc1db05cf39990f3cf
li-kuan-gi <82380617+li-kuan-gi@users.noreply.github.com>
2022-06-19 04:53:57 +0000

    change model from resnet to mobilenet (v3, quantized, large)

--- model-train/model.py
+++ model-train/model.py
@@ -3,9 +3,9 @@ from torchvision import models
 
 
 def get_model(output_size):
-    model = models.densenet121(pretrained=True)
+    model = models.quantization.mobilenet_v3_large(pretrained=True)
 
-    n_feats = model.classifier.in_features
-    model.classifier = torch.nn.Linear(n_feats, output_size)
+    n_feats = model.classifier[-1].in_features
+    model.classifier[-1] = torch.nn.Linear(n_feats, output_size)
 
     return model
